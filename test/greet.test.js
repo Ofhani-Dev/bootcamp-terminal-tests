3e042f82db516d0dd9596a871e91928f85cabcd0
Ofhani-Dev <omukwevho3@gmail.com>
2021-05-12 21:46:55 +0200

    my first test commit

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,1 +1,15 @@
-var greet = require("../greet")
+var assert = require("assert");
+var greet = require("../greet");
+
+describe('This test should greet a  friend.' , function(){
+
+    it( 'should greet "Hello, Tim" ', function(){
+        assert.equal("Hello, Tim", greet("Tim"));
+    });
+    it('should return "Hello, Zamo" ' , function(){
+        assert.equal("Hello, Zamo", greet("Zamo"));
+    });
+    it('should return "Hello, khakhathi" ' , function(){
+        assert.equal("Hello, Khakhathi", greet("Khakhathi"));
+    });
+});
